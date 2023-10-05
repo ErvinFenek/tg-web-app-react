19ed191909fec7061a16d5fdc83be325fcd76c02
Anton Shuhov <antonshuhov@gmail.com>
2023-10-05 13:42:33 +0300

    commit 05 20 2023 - 3

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,7 +3,7 @@ import "./Form.css";
 import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
-    const [tg] = useTelegram();
+    const {tg} = useTelegram();
 
     const [country, setCountry] = useState("");
     const [city, setCity] = useState("");
@@ -38,17 +38,19 @@ const Form = () => {
     }
 
     return (
-        <div className={'form'}>
-            <h3>Введите ваши данные</h3>
-            <input className={'input'} type="text" placeholder={"Страна"} value={country} onChange={onChangeCountry}/>
-            <input className={'input'} type="text" placeholder={"Город"} value={city} onChange={onChangeCity}/>
-            <input className={'input'} type="text" placeholder={"Улица"} value={street} onChange={onChangeStreet}/>
+        <>
+            <div className={'form'}>
+                <h3>Введите ваши данные</h3>
+                <input className={'input'} type="text" placeholder={"Страна"} value={country} onChange={onChangeCountry}/>
+                <input className={'input'} type="text" placeholder={"Город"} value={city} onChange={onChangeCity}/>
+                <input className={'input'} type="text" placeholder={"Улица"} value={street} onChange={onChangeStreet}/>
 
-            <select value={subject} onChange={onChangeSubject} className={"select"}>
-                <option value={'physical'}>Физ. лицо</option>
-                <option value={'legal'}>Юр. лицо</option>
-            </select>
-        </div>
+                <select value={subject} onChange={onChangeSubject} className={"select"}>
+                    <option value={'physical'}>Физ. лицо</option>
+                    <option value={'legal'}>Юр. лицо</option>
+                </select>
+            </div>
+        </>
     );
 };
 
