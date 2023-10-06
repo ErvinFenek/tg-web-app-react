44c61fc3b9d63072401495013c85e60b687d52f8
Anton Shuhov <antonshuhov@gmail.com>
2023-10-06 17:15:33 +0300

    commit 06 10 2023 - 7

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -25,7 +25,7 @@ const ProductList = () => {
     const [addItems, setAddItems] = useState([]);
     const {tg} = useTelegram();
 
-    const onAddHandler = () => {
+    const onAdd = () => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
 
